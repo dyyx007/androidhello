83655d4b345abb4c069dbf0a4bf9d73bb2cb981a
井空 <gang.dug@alibaba-inc.com>
2016-03-04 17:07:30 +0800

    拼图潜质竖屏

--- project/src/com/dyyx/androidhello/MainActivity.java
+++ project/src/com/dyyx/androidhello/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends BaseActivity {
 	
 	private static Map<String,Class> activityMap = new HashMap<String,Class>();
 	
-	private static final String VERSION = "20160304-1655";
+	private static final String VERSION = "20160304-1705";
 	
 	static{
 		
